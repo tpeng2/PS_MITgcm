e999d97454407bd0b91d0fe9f6a0ffaad04e39ea
Tianze Peng <tianze.peng@mcgill.ca>
2022-02-23 13:28:04 -0500

    code clear-up

--- py_process/code/readPlot_spec_pkl.py
+++ py_process/code/readPlot_spec_pkl.py
@@ -38,32 +38,53 @@ class spectraPKL():
     keHead = 'KE'
     divHead = 'DIV'
     zetaHead = 'ZETA'
-    zetaGHead = 'ZETA_geo'
-    zetaAGHead = 'ZETA_ageo'
-    lapPHead = 'Lap_P'
+    zetaGHead = 'ZETA_G'
+    zetaAGHead = 'dZETA'
+    lapPHead = 'Pk5'
     # make a list
     self.fieldHeadList = [keHead, divHead, zetaHead, zetaGHead, zetaAGHead, lapPHead]
     # three locations strings
     self.locStrList = ['surf','sekm', '-200m']
 
-  def loadPKL(self):
-    # construct file name
-    appendixStr = 'days.pkl'
-    self.fieldnameArrL = [['']*len(self.fieldHeadList)]*len(self.locStrList)
-    self.spectra = [['']*len(self.fieldHeadList)]*len(self.locStrList)
-    # format: field_caseame_appendix
-    ifield = 0
-    for field in self.fieldHeadList:
-      iloc = 0
-      for loc in self.locStrList:
-        self.fieldnameArrL[iloc][ifield] = '_'.join([self.fieldHeadList[ifield], \
-                                                     self.locStrList[iloc],self.casename,'{:d}'.format(int(self.tDuration)),appendixStr])
-        self.specPKL[iloc][ifield] = load_pickle(self.pklPath + self.fieldnameArrL[iloc][ifield],'rb')
-        iloc+=1;
-      ifield+=1;
+  def unzip_pickle(self, indLoc, indField, clmin, clmax):
+    appendixStr = 'days.pkl'''
+    specPKL = [['']*len(self.fieldHeadList)]*len(self.locStrList)
+    fieldnameArrL = '_'.join([self.fieldHeadList[indField], \
+                  self.locStrList[indLoc],self.casename,'{:d}'.format(int(self.tDuration)),appendixStr])
+    specPKL = load_pickle(self.pklPath + fieldnameArrL,'rb')
+    k1d = 0.5*(specPKL['binedge_kappa'][0:-1] + specPKL['binedge_kappa'][1:])*1000
+    omg = specPKL['omg_eff']
+    specMat = specPKL['KE_kappaT_eff']
+    # return k1d,omg,specMat
+    K,W=np.meshgrid(k1d,omg)
+    plt.figure()
+    plt.pcolormesh(k1d,omg,tch.log(tch.tensor(W)*tch.tensor(K)*specMat)/tch.log(tch.tensor(10.0)),cmap = 'terrain')
+    plt.colorbar()
+    plt.title(self.fieldHeadList[indField]+', '+self.locStrList[indLoc])
+    plt.clim(clmin,clmax)
+    plt.yscale('log'),plt.ylim([0.06,4])
+    plt.xscale('log'),plt.xlim([0.004,k1d.max()/np.sqrt(2.0)])
+    plt.suptitle(self.casename)
 
 #%%
 pathSpec = home_dir+'/postproc/img';
 
-spectra_tp16uvar9 = spectraPKL('tp16_uvar_9',37,pathSpec)
-spectra_tp16uvar9.loadPKL();
+clmin = 4
+clmax = 10
+
+for indLoc in range(3):
+  for indField in range(5):
+    # del spectra_case
+    spectra_case = spectraPKL('ex16_ucon_9', 36, pathSpec) # spectra_tp16uvar9.loadPKL();
+    spectra_case.unzip_pickle(indLoc, indField, clmin, clmax)    
+    
+    del spectra_case
+    spectra_case = spectraPKL('ex16_uvar_9', 36, pathSpec) # spectra_tp16uvar9.loadPKL()
+    spectra_case.unzip_pickle(indLoc, indField, clmin, clmax)
+    
+    spectra_case = spectraPKL('tp16_uvar_9', 37, pathSpec)# spectra_tp16uvar9.loadPKL();
+    spectra_case.unzip_pickle(indLoc, indField, clmin, clmax)
+    
+    del spectra_case
+    spectra_case = spectraPKL('tp16_ucon_9', 36, pathSpec)# spectra_tp16uvar9.loadPKL();
+    spectra_case.unzip_pickle(indLoc, indField, clmin, clmax)
